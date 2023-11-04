430acc5ddfff13387e07ad5b70976d59eb02ead7
Ansari Anas <ansariii.5130@gmail.com>
2023-11-04 21:01:05 +0530

    "made few chnges on navbar"

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,4 @@
 import Navbar from './Navbar'
-import NavBarTRy from './NavBarTRy'
 import Footer from './Footer'
 import { Outlet } from 'react-router-dom'
 import  Container  from '../components/Container'
@@ -11,8 +10,7 @@ function Layout() {
     <div className='bg-[#f1f2f4]'>
         <div className='w-full bg-flipkart-blue sticky top-0 z-20'>
           <Container>
-            {/* <Navbar/> */}
-            <NavBarTRy/>
+            <Navbar/>
           </Container>
         </div>
 
