e934bff6e2f3094171cc597ffa65f350b0504361
Ansari Anas <111463267+ansarianas23@users.noreply.github.com>
2024-02-09 01:49:48 +0530

    Product Slider updated

--- src/components/ProductSlider.jsx
+++ src/components/ProductSlider.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import ProductCard from './ProductCard'
 import { useSelector } from 'react-redux'
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 
 function ProductSlider({title}) {
 
@@ -33,42 +34,46 @@ function ProductSlider({title}) {
     }
 };
 
+
   const products = useSelector(state => state.product.products);
 
+  const sliderRef = useRef();
+  const scrollAmount = 200;
+
+  const movePrevious = ()=>{
+    sliderRef.current.scrollLeft -= scrollAmount;
+  }
+
+  const moveNext = () =>{
+    sliderRef.current.scrollLeft += scrollAmount;
+  }
+
   return (
-    <div className='w-full h-full py-5 px-6'>
-        <h1 className='text-2xl font-semibold pb-5'>{title}</h1>
-          <Carousel
-            swipeable={false}
-            draggable={false}
-            responsive={responsive}
-            // autoPlay={true}
-            // autoPlaySpeed={10000}
-            // centerMode={true}
-            // removeArrowOnDeviceType={["tablet", "mobile"]}
-            // centerMode={true}
-            infinite={true}
-            keyBoardControl={true}
-            showDots={false}
-            containerClass="carousel-container"
-            dotListClass="custom-dot-list-style"
-            itemClass="carousel-item-padding-40-px"
-          >
-            {products.map((eachItem) => (
-                <ProductCard
-                key={eachItem.title}
-                title={eachItem.title}
-                desc={eachItem.desc}
-                price={eachItem.price}
-                mrp={eachItem.mrp}
-                imageUrl={eachItem.imageUrl}
-                id={eachItem.id}
-                seller = {eachItem.seller}
-                height={"250px"}
-                width={"200px"}
-                />
-                ))}
-        </Carousel>
+    <div className='w-full h-full pb-3 md:pb-5 px-3 md:px-6 relative'>
+        <h1 className='text-lg md:text-xl font-semibold py-3 md:py-5'>{title}</h1>
+
+        <div ref={sliderRef} className='w-full flex gap-2 md:gap-4 overflow-x-auto scrollbar-hide scroll-smooth'>
+          {products.map((eachItem) => (
+            <ProductCard
+            key={eachItem.title}
+            title={eachItem.title}
+            desc={eachItem.desc}
+            price={eachItem.price}
+            mrp={eachItem.mrp}
+            imageUrl={eachItem.imageUrl}
+            id={eachItem.$id}
+            seller = {eachItem.seller}
+            height="250px"
+            width="200px"
+            />
+            ))}
+        </div>
+
+        {/* Slider Buttons */}
+          <div className='w-full h-fit flex justify-between absolute left-0 top-[43%]'>
+            <span onClick={movePrevious} className='bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)] md:px-3 px-2 py-7 md:py-9 rounded-r-sm cursor-pointer'><FaAngleLeft /></span>
+            <span onClick={moveNext} className='bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)] md:px-3 px-2 py-7 md:py-9 rounded-l-sm cursor-pointer'><FaAngleRight /></span>
+          </div>
     </div>
   )
 }
